614f7b4a2fd509604a6b95d563c7c60c90c2f651
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-06-16 00:38:16 +0200

    Improve preview responsiveness

--- preview/tailwind.config.js
+++ preview/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./**/*.html", "./**/.cshtml", "./**/*.razor"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '-xl': { 'max': '1279px' },
+      '-lg': { 'max': '1023px' },
+      '-md': { 'max': '767px' },
+      '-sm': { 'max': '639px' },
+    }
   },
   plugins: [],
 }

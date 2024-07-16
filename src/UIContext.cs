3d085c77a54fa1b5d6e6d54dd2c6b3151365541b
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-07-16 02:54:46 +0200

    Add Menu property to UIContext record

--- src/UIContext.cs
+++ src/UIContext.cs
@@ -1,6 +1,10 @@
+using ChatAIze.DopamineUI.Components;
+
 namespace ChatAIze.DopamineUI;
 
 public sealed record UIContext
 {
+    public DPMenu? Menu { get; init; }
+
     public bool IsDisabled { get; init; }
 }

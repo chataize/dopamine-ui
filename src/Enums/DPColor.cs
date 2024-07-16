f9049b1942d3d310ee639712d15fede986defe6a
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-07-17 01:32:23 +0200

    Add DPColor enum

---
+++ src/Enums/DPColor.cs
@@ -0,0 +1,48 @@
+namespace ChatAIze.DopamineUI.Enums;
+
+public enum DPColor
+{
+    Slate,
+
+    Gray,
+
+    Zinc,
+
+    Neutral,
+
+    Stone,
+
+    Red,
+
+    Orange,
+
+    Amber,
+
+    Yellow,
+
+    Lime,
+
+    Green,
+
+    Emerald,
+
+    Teal,
+
+    Cyan,
+
+    Sky,
+
+    Blue,
+
+    Indigo,
+
+    Violet,
+
+    Purple,
+
+    Fuchsia,
+
+    Pink,
+
+    Rose
+}

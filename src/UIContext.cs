67b943da78a9fd4b4610bab44c737e7c034eda74
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-07-19 02:45:02 +0200

    Add NavigationView property to UIContext record

--- src/UIContext.cs
+++ src/UIContext.cs
@@ -4,6 +4,8 @@ namespace ChatAIze.DopamineUI;
 
 public sealed record UIContext
 {
+    public DPNavigationView? NavigationView { get; init; }
+
     public DPTabView? TabView { get; init; }
 
     public DPMenu? Menu { get; init; }

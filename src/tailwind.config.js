ddce2102cfa18adc88546279f4d4112289a65963
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-07-19 19:33:00 +0200

    Add 2xl and -2xl screens

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
+      '2xl': '1536px',
+      '-2xl': { 'max': '1535px' },
       '-xl': { 'max': '1279px' },
       '-lg': { 'max': '1023px' },
       '-md': { 'max': '767px' },

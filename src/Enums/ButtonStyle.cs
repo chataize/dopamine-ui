820148473e70d5f8249a7db9b3655935ba5a80a8
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-07-20 02:06:38 +0200

    Add rounded button style

--- src/Enums/ButtonStyle.cs
+++ src/Enums/ButtonStyle.cs
@@ -3,10 +3,18 @@ namespace ChatAIze.DopamineUI.Enums;
 public enum ButtonStyle
 {
     Default,
+
+    Rounded,
+
     Borderless,
+
     Light,
+
     Primary,
+
     Accent,
+
     Danger,
+
     Destructive
 }

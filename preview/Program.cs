1f448b57ac18fa3ea3770f77f8aa5fec5486c88d
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-14 22:24:16 +0100

    Use MapStaticAssets instead of UseStaticFiles

--- preview/Program.cs
+++ preview/Program.cs
@@ -12,7 +12,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.MapStaticAssets();
 app.UseAntiforgery();
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 app.Run();

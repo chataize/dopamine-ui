db7fad1a264ef7101c226947dc0c60d7f4a70510
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-04-22 13:59:19 +0200

    Update push-templates-nuget.sh to remove symbols

--- tools/push-templates-nuget.sh
+++ tools/push-templates-nuget.sh
@@ -5,9 +5,6 @@ cd ../templates
 dotnet pack
 
 nupkg=$(ls ./bin/Release/ChatAIze.DopamineUI.Templates.*.nupkg | grep -v '\.snupkg$' | sort -V | tail -n1)
-snupkg="${nupkg%.nupkg}.snupkg"
 
 dotnet nuget push "$nupkg" --api-key "$CHATAIZE_NUGET_API_KEY" --source "https://api.nuget.org/v3/index.json" --skip-duplicate
-dotnet nuget push "$snupkg" --api-key "$CHATAIZE_NUGET_API_KEY" --source "https://api.nuget.org/v3/index.json" --skip-duplicate
 dotnet nuget push "$nupkg" --api-key "$GITHUB_PAT" --source "https://nuget.pkg.github.com/chataize/index.json" --skip-duplicate
-dotnet nuget push "$snupkg" --api-key "$GITHUB_PAT" --source "https://nuget.pkg.github.com/chataize/index.json" --skip-duplicate
